105d93bdc5c44fc89a23b2398a7d9d6f9eff01a4
A5b84 <42934711+A5b84@users.noreply.github.com>
2022-05-22 16:23:55 +0200

    Update to 1.19-pre1

--- src/main/java/io/github/a5b84/statuseffectbars/mixin/StatusEffectInstanceMixin.java
+++ src/main/java/io/github/a5b84/statuseffectbars/mixin/StatusEffectInstanceMixin.java
@@ -18,9 +18,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class StatusEffectInstanceMixin implements StatusEffectInstanceDuck {
 
     @Unique private int maxDuration;
-    @Shadow private int duration;
+    @Shadow int duration;
 
-    @Inject(method = "<init>(Lnet/minecraft/entity/effect/StatusEffect;IIZZZLnet/minecraft/entity/effect/StatusEffectInstance;)V",
+    @Inject(method = "<init>(Lnet/minecraft/entity/effect/StatusEffect;IIZZZLnet/minecraft/entity/effect/StatusEffectInstance;Ljava/util/Optional;)V",
             at = @At("RETURN"))
     private void onInit(CallbackInfo ci) {
         maxDuration = duration;
